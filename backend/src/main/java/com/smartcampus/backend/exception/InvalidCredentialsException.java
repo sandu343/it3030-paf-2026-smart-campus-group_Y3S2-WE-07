ef37411313cb99e0f00ed2f7eca54be28f90ee51
sandu343 <shansika343@gmail.com>
2026-04-06 11:40:00 +0530

    add global and auth validation exception files

---
+++ backend/src/main/java/com/smartcampus/backend/exception/InvalidCredentialsException.java
@@ -0,0 +1,11 @@
+package com.smartcampus.backend.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+    
+    public InvalidCredentialsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
