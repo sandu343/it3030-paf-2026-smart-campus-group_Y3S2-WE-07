9c1b01724d9ea4113d829c4c2f439efb55e968ea
Imashaherath <imashaherath83@gmail.com>
2026-04-21 11:40:00 +0530

    update 2 model files

---
+++ backend/src/main/java/com/smartcampus/backend/model/NotificationType.java
@@ -0,0 +1,11 @@
+package com.smartcampus.backend.model;
+
+public enum NotificationType {
+    BOOKING_CREATED,
+    BOOKING_APPROVED,
+    BOOKING_REJECTED,
+    TICKET_CREATED,
+    TICKET_ASSIGNED,
+    TICKET_STATUS_UPDATED,
+    COMMENT_ADDED
+}
